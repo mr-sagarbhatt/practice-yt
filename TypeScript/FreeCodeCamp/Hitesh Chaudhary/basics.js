37a12886596528a15f79413a2ffe6b91569355f4
sagar <bhattsagar112@gmail.com>
2023-03-19 21:40:25 +0530

    19-03-23 | FreeCodeCamp/HiteshChaudhary/TypeScript | Added functions

--- TypeScript/FreeCodeCamp/Hitesh Chaudhary/basics.js
+++ TypeScript/FreeCodeCamp/Hitesh Chaudhary/basics.js
@@ -1,61 +1,52 @@
+"use strict";
 // TODO: 1.Variables | Data Types
 // ? syntax: let variableName: type = value
 // ? JavaScript has eight data types. Seven primitive types and one object Data type. The primitive types are number, string, boolean, bigint, symbol, undefined, and null. Everything else is an object in JavaScript.
 // ? The TypeScript Type System supports all of them and also brings its own special types. They are unknown, any, void & never.
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // TODO: Primitive data types: string, number, boolean
 // * string
-let greetings: String = 'Hello Sagar'
+var greetings = 'Hello Sagar';
 // greetings = 6 // ! Typescript gives an error
-greetings.toLowerCase()
-console.log(greetings)
-
+greetings.toLowerCase();
+console.log(greetings);
 // * number
-let userId: number = 1
+var userId = 1;
 // userId.toUpperCase() // ! Typescript gives an error
-console.log(userId)
-
+console.log(userId);
 // * boolean
-let isLoggedIn: boolean = true
-console.log(isLoggedIn)
-
+var isLoggedIn = true;
+console.log(isLoggedIn);
 // TODO: Type Inference
 // ? In TypeScript, there are several places where type inference is used to provide type information when there is no explicit type annotation.
-let myNum = 6 // no explicit type annotation | We have already initialized a value
+var myNum = 6; // no explicit type annotation | We have already initialized a value
 // ? The type of the myNum variable is inferred to be number. This kind of inference takes place when initializing variables and members, setting parameter default values, and determining function return types.
 // myNum = 'sagar' // ! Typescript gives an error, however we have not defined the explicit type annotation
-
-myNum.toFixed(2)
-console.log(myNum)
-
+myNum.toFixed(2);
+console.log(myNum);
 // TODO: Special data types: any, unknown, never, undefined & null
-
 // * any
 // ? If we don't supply a type, it will be of type any by default.
-
-let hero // ? inferred to be type any, such case define explicit type annotation
+var hero; // ? inferred to be type any, such case define explicit type annotation
 // let hero: string // ? explicit type annotation
-const getHero = () => 'Thor'
+var getHero = function () { return 'Thor'; };
 // const getHero = () => true // ! Typescript gives an error
-hero = getHero()
-console.log(hero)
-
+hero = getHero();
+console.log(hero);
 // TODO: Functions
-const addTwo = (num: number) => {
-  // num.toUpperCase() // ! Typescript will not gives an error, If not provided explicit type annotation in function definition for parameters
-  return 'hello' // ! Typescript will not gives an error, If not provided explicit type annotation in function definition for return type
-  // return num + 2
-}
-const resAddTwo = addTwo(5)
-// const resAddTwo = addTwo('5') // ! Typescript will not gives an error, If not provided explicit type annotation in function definition for parameters
-console.log(resAddTwo)
-
-// * Default Values
-const logInUser = (name: string, email: string, isPaid: boolean = false) => {
-  return name
-}
-const resLogInUser = logInUser('Sagar', 'bhattsagar112@gmail.com')
+var addTwo = function (num) {
+    // num.toUpperCase() // ! Typescript will not gives an error, If not provided explicit type annotation in function definition
+    return 'hello';
+    // return num + 2
+};
+var resAddTwo = addTwo(5);
 // const resAddTwo = addTwo('5') // ! Typescript will not gives an error, If not provided explicit type annotation in function definition
-console.log(resLogInUser)
-
-export {}
+console.log(resAddTwo);
+// * Default Values
+var logInUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+    return name;
+};
+var resLogInUser = logInUser('Sagar', 'bhattsagar112@gmail.com');
+// const resAddTwo = addTwo('5') // ! Typescript will not gives an error, If not provided explicit type annotation in function definition
+console.log(resLogInUser);
--- TypeScript/typescript.md
+++ TypeScript/typescript.md
@@ -38,18 +38,33 @@
     - Type Safety is missing in javascript
     - Ex: If you add number with string value (1 + '1' = 11 | null + 1 = 1 | undefined + 1 = NaN)
 
-## Types
+## [_*Data Types*_](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html)
 
-    Number, String, Boolean, Null, Undefined, Void, Object, Array, Tuples
-    Any, Never, unknown
+    Number, String, Boolean, Null, Undefined, Object, Array, Tuples, Any, Never, Void, unknown
+
+    Primitive data types: string, number, boolean
+    Special data types: any, unknown, never, undefined & null
+
+    JavaScript has eight data types. Seven primitive types and one object Data type. The primitive types are number, string, boolean, bigint, symbol, undefined, and null. Everything else is an object in JavaScript.
+    The TypeScript Type System supports all of them and also brings its own special types. They are unknown, any, void & never.
+
+#### [_*Type Inference*_](https://www.typescriptlang.org/docs/handbook/type-inference.html)
+
+    In TypeScript, there are several places where type inference is used to provide type information when there is no explicit type annotation.
+    Ex: let myNum = 6 | Here we have no explicit type annotation | We have already initialized a value
+    The type of the myNum variable is inferred to be number. This kind of inference takes place when initializing variables and members, setting parameter default values, and determining function return types.
 
 # Two ways of installation (Globally | Locally)
 
     - Globally : For all projects | Installed in system. Used it for learning
-    - Locally : For projects we required typescript config file for configuration
+    - Locally : For projects we required ts config file for configuration
 
 # Points To Remember
 
-    Any: Don't use type Any, If possible
+## Types
+
+    Any: Not a good practice to use type Any, If possible specify the type. If we don't supply a type, it will be of type any by default and we can change it through ts config. file
+    Number: JavaScript does not have a special runtime value for integers, so there’s no equivalent to int or float - everything is simply number.
+    Type Inference: takes place when initializing variables and members, setting parameter default values, and determining function return types. For other scenarios we need to define explicit type annotation else type will be any by default.
 
 </samp>
